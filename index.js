a9c92321bc3dfb4fe7ee34848f3ec70a76133da5
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 10:29:33 -0600

    fixed server port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = 3002;
 
 const path = require('path');
 app.use(bodyParser.json());
-app.use('/gaia', express.static(path.join(__dirname, 'dist/')));
+app.use('/remnant', express.static(path.join(__dirname, 'dist/')));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
