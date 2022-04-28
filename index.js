89afd562783e84aeec7befb9860ca14eb14b4fdc
Heine574 <johannes@niemela.se>
2022-04-28 15:49:37 -0500

    fixed legacy page to be served under the /archive endpoint

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const ADDR_PREFIX = '/remnant';
 
 const path = require('path');
 app.use(bodyParser.json());
-app.use(`${ADDR_PREFIX}/`, express.static(path.join(__dirname, 'archive/')));
+app.use(`${ADDR_PREFIX}/archive`, express.static(path.join(__dirname, 'archive/')));
+
+app.get(`${ADDR_PREFIX}/`, (req, res) => {
+  res.redirect(`${ADDR_PREFIX}/archive`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
