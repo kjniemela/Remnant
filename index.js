777a0a10eee9e0f139065f3e4d10bd4fbb65bc62
Heine574 <johannes@niemela.se>
2022-04-29 03:54:33 -0500

    started work on the atlas page template

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const pug = require('pug');
@@ -9,17 +11,26 @@ const ADDR_PREFIX = '/remnant';
 const app = express();
 app.use(bodyParser.json());
 
+// load json data
+const countries = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/countries.json')));
+console.log(countries);
+
 // compile templates
 const homeTemplate = pug.compileFile('templates/home.pug');
+const atlasTemplate = pug.compileFile('templates/atlas.pug');
 
-const path = require('path');
 app.use(favicon(path.join(__dirname, 'assets/favicon.ico')));
 app.use(`${ADDR_PREFIX}/archive`, express.static(path.join(__dirname, 'archive/')));
 app.use(`${ADDR_PREFIX}/assets`, express.static(path.join(__dirname, 'assets/')));
 
 app.get(`${ADDR_PREFIX}/`, (req, res) => {
-  const html = homeTemplate({
-    name: 'World'
+  const html = homeTemplate();
+  res.end(html);
+});
+
+app.get(`${ADDR_PREFIX}/atlas`, (req, res) => {
+  const html = atlasTemplate({
+    countries
   });
   res.end(html);
 });
