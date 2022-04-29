5705566e858db55cf8bb699bc1e8d8bd6c5938e6
Heine574 <johannes@niemela.se>
2022-04-28 21:45:35 -0500

    added pug templates for home page

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const favicon = require('serve-favicon');
+const pug = require('pug');
 
 const PORT = 3002;
 const ADDR_PREFIX = '/remnant';
 
-const path = require('path');
+const app = express();
 app.use(bodyParser.json());
+
+// compile templates
+const homeTemplate = pug.compileFile('templates/home.pug');
+
+const path = require('path');
+app.use(favicon(path.join(__dirname, 'assets/favicon.ico')));
 app.use(`${ADDR_PREFIX}/archive`, express.static(path.join(__dirname, 'archive/')));
+app.use(`${ADDR_PREFIX}/assets`, express.static(path.join(__dirname, 'assets/')));
 
 app.get(`${ADDR_PREFIX}/`, (req, res) => {
-  res.redirect(`${ADDR_PREFIX}/archive`);
+  const html = homeTemplate({
+    name: 'World'
+  });
+  res.end(html);
 });
 
 app.listen(PORT, () => {
