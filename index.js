160c25543af669de95d05688ee9a8e033034fe09
Heine574 <johannes@niemela.se>
2022-05-07 17:13:23 -0500

    added pug templates for atlas section

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ const countries = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/countrie
 console.log(countries);
 
 // compile templates
+const errorNotFoundTemplate = pug.compileFile('templates/notFound.pug');
 const homeTemplate = pug.compileFile('templates/home.pug');
 const atlasTemplate = pug.compileFile('templates/atlas.pug');
+const countriesTemplate = pug.compileFile('templates/countries.pug');
+const countryTemplate = pug.compileFile('templates/country.pug');
 
 app.use(favicon(path.join(__dirname, 'assets/favicon.ico')));
 app.use(`${ADDR_PREFIX}/archive`, express.static(path.join(__dirname, 'archive/')));
@@ -32,6 +35,28 @@ app.get(`${ADDR_PREFIX}/atlas`, (req, res) => {
   const html = atlasTemplate({
     countries
   });
+  res.end(html);
+});
+
+app.get(`${ADDR_PREFIX}/atlas/countries`, (req, res) => {
+  const html = countriesTemplate({
+    countries
+  });
+  res.end(html);
+});
+
+app.get(`${ADDR_PREFIX}/atlas/countries/:country`, (req, res) => {
+  const country = countries[req.params.country];
+  let html;
+  if (country) {
+    html = countryTemplate({
+      ...country,
+      pathName: req.params.country
+    });
+  } else {
+    res.status(404);
+    html = errorNotFoundTemplate();
+  }
   res.end(html);
 });
 
